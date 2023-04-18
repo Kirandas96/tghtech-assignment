98303de99a68676d5827bdb01df611f68c6c6a17
Kirandas96 <parvathikiran10@gmail.com>
2023-04-18 15:03:16 +0530

    done

--- src/Components/Bookmark.jsx
+++ src/Components/Bookmark.jsx
@@ -1,11 +1,27 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Loading } from "./Loading";
 import { Navbar } from "./Navbar";
+import { getQuotes} from "../Redux/Quotes/action";
 
 export const Bookmark = () => {
+  const[c,setC]=useState(1)
+const dispatch=useDispatch()
   const saved = useSelector((state) => state.quoteReducer.bookmarks);
   const loading=useSelector((state) => state.quoteReducer.isLoading);
+  const bookmarks=localStorage.getItem("bookmarks")!==null?JSON.parse(localStorage.getItem("bookmarks")):[]
+  var filter=[]
+  console.log(saved);
+
+useEffect(()=>{
+  dispatch(getQuotes())
+},[c])
+  const handleRemove=(el)=>{
+    filter=bookmarks.filter(item=>item._id!==el._id)
+    localStorage.clear();
+    localStorage.setItem("bookmarks",JSON.stringify(filter))
+    setC(c+1)
+  }
   return (
     <div>
       <div className="home">
@@ -22,6 +38,7 @@ export const Bookmark = () => {
               <div className="quote" key={el._id}>
                 <p>{el.content}</p>
                 <h3>-{el.author}</h3>
+                <button onClick={()=>handleRemove(el)}>Remove</button>
               </div>
             );
           })}
